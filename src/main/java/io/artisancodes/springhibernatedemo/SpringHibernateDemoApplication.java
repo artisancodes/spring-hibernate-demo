38173d7a1be44e75cdcf7681655b6b4047cd6f1b
Rúben Leonardo <rleonardo.pt@gmail.com>
2023-09-22 22:18:41 +0100

    Initial commit

---
+++ src/main/java/io/artisancodes/springhibernatedemo/SpringHibernateDemoApplication.java
@@ -0,0 +1,13 @@
+package io.artisancodes.springhibernatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateDemoApplication.class, args);
+	}
+
+}
