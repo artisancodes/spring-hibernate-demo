b4821d177408fc743ddd49b50075670b5463951e
Rúben Leonardo <rleonardo.pt@gmail.com>
2023-09-22 23:25:02 +0100

    Use Qualifier annotation

--- src/main/java/io/artisancodes/springhibernatedemo/common/TennisCoach.java
+++ src/main/java/io/artisancodes/springhibernatedemo/common/TennisCoach.java
@@ -3,9 +3,9 @@ package io.artisancodes.springhibernatedemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach {
+public class TennisCoach implements Coach {
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes :)";
+        return "Practice your backhand volley";
     }
 }
--- src/main/java/io/artisancodes/springhibernatedemo/rest/DemoController.java
+++ src/main/java/io/artisancodes/springhibernatedemo/rest/DemoController.java
@@ -2,18 +2,27 @@ package io.artisancodes.springhibernatedemo.rest;
 
 import io.artisancodes.springhibernatedemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Autowiring
+ * - Injecting a Coach implementation
+ * - Spring will scan @Components
+ * - Any one implements Coach interface
+ * - If so, let's inject them... oops which one?
+ *
+ * Use @Qualifier annotation to specify the bean id: cricketCoach
+ * Same name of the class, first character lower-case
+ */
 @RestController
 public class DemoController {
 
-    // define a private field for the dependency
     private Coach myCoach;
 
     @Autowired
-    // We can use any method name
-    public void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
