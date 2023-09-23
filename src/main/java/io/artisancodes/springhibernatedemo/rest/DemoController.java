783d3e299938e8053dca82a774921ded350e4a48
Rúben Leonardo <rleonardo.pt@gmail.com>
2023-09-23 12:02:24 +0100

    Use Primary annotation

--- src/main/java/io/artisancodes/springhibernatedemo/rest/DemoController.java
+++ src/main/java/io/artisancodes/springhibernatedemo/rest/DemoController.java
@@ -2,27 +2,16 @@ package io.artisancodes.springhibernatedemo.rest;
 
 import io.artisancodes.springhibernatedemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Autowiring
- * - Injecting a Coach implementation
- * - Spring will scan @Components
- * - Any one implements Coach interface
- * - If so, let's inject them... oops which one?
- *
- * Use @Qualifier annotation to specify the bean id: cricketCoach
- * Same name of the class, first character lower-case
- */
 @RestController
 public class DemoController {
 
     private Coach myCoach;
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    public DemoController(Coach theCoach) {
         myCoach = theCoach;
     }
 
