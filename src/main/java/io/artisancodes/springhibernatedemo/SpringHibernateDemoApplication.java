844f53054fc067bec1c6ee117c0c380716c8d0ea
Rúben Leonardo <rleonardo.pt@gmail.com>
2023-09-25 20:46:04 +0100

    Saving single student object

--- src/main/java/io/artisancodes/springhibernatedemo/SpringHibernateDemoApplication.java
+++ src/main/java/io/artisancodes/springhibernatedemo/SpringHibernateDemoApplication.java
@@ -1,13 +1,38 @@
 package io.artisancodes.springhibernatedemo;
 
+import io.artisancodes.springhibernatedemo.entity.Student;
+import io.artisancodes.springhibernatedemo.entity.dao.StudentDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringHibernateDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringHibernateDemoApplication.class, args);
+	}
+
+	// Executed after Spring Beans have been loaded.
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> {
+			createStudent(studentDAO) ;
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create a student object
+		System.out.println("Creating a new student object ...");
+		Student student = new Student("Rúben", "Leonardo", "ruben@artisancodes.io");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(student);
+
+		// display the id of the saved student
+		System.out.println("Saved student. Generated ID: " + student.getId());
 	}
 
 }
