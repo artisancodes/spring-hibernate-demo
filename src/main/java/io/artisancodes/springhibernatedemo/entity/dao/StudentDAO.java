a6445803d422cad5fc1b860256eee9c7e644b63e
Rúben Leonardo <rleonardo.pt@gmail.com>
2023-09-25 20:45:49 +0100

    Implement StudentDAO

---
+++ src/main/java/io/artisancodes/springhibernatedemo/entity/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package io.artisancodes.springhibernatedemo.entity.dao;
+
+import io.artisancodes.springhibernatedemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
---
+++ src/main/java/io/artisancodes/springhibernatedemo/entity/dao/StudentDAOImpl.java
@@ -0,0 +1,28 @@
+package io.artisancodes.springhibernatedemo.entity.dao;
+
+import io.artisancodes.springhibernatedemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    // define the field for entity manager
+    private final EntityManager entityManager;
+
+    // inject entity manager using constructor
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    // add transaction annotation since we are performing an update
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+}
