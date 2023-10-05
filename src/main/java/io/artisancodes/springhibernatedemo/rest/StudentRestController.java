553087bdfa23872c12554d60b36cbc437b21c8a7
Rúben Leonardo <rleonardo.pt@gmail.com>
2023-10-05 21:45:16 +0100

    Read a Student by ID

--- src/main/java/io/artisancodes/springhibernatedemo/rest/StudentRestController.java
+++ src/main/java/io/artisancodes/springhibernatedemo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package io.artisancodes.springhibernatedemo.rest;
 
 import io.artisancodes.springhibernatedemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +14,25 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load data the student data ... only once!
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Rúben", "Leonardo"));
+        theStudents.add(new Student("Jéssica", "Fonseca"));
+    }
+
     // Define endpoint for "/students" - returns a list of students
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Rúben", "Leonardo"));
-        students.add(new Student("Jéssica", "Fonseca"));
-       return students;
+        return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable int studentId) {
+        // just index into the list... keep it simple for now
+        return theStudents.get(studentId);
     }
 }
