c8c8bc15040bad21ad4dc3c0fd4bd10c9e8f8f15
Rúben Leonardo <rleonardo.pt@gmail.com>
2023-10-05 22:29:07 +0100

    REST Exception Handling - use @ControllerAdvice (best practice)

--- src/main/java/io/artisancodes/springhibernatedemo/rest/StudentRestController.java
+++ src/main/java/io/artisancodes/springhibernatedemo/rest/StudentRestController.java
@@ -2,9 +2,6 @@ package io.artisancodes.springhibernatedemo.rest;
 
 import io.artisancodes.springhibernatedemo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,24 +36,5 @@ public class StudentRestController {
             throw new StudentNotFoundException("Student id not found: " + studentId);
         }
         return theStudents.get(studentId);
-    }
-
-    // Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
-        StudentErrorResponse response = new StudentErrorResponse();
-        response.setStatus(HttpStatus.NOT_FOUND.value());
-        response.setMessage(ex.getMessage());
-        response.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception ex) {
-        StudentErrorResponse response = new StudentErrorResponse();
-        response.setStatus(HttpStatus.BAD_REQUEST.value());
-        response.setMessage(ex.getMessage());
-        response.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(response, HttpStatus.BAD_GATEWAY);
     }
 }
---
+++ src/main/java/io/artisancodes/springhibernatedemo/rest/StudentRestExceptionHandler.java
@@ -0,0 +1,28 @@
+package io.artisancodes.springhibernatedemo.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentRestExceptionHandler {
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
+        StudentErrorResponse response = new StudentErrorResponse();
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setMessage(ex.getMessage());
+        response.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception ex) {
+        StudentErrorResponse response = new StudentErrorResponse();
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setMessage(ex.getMessage());
+        response.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.BAD_GATEWAY);
+    }
+}
